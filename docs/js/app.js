936c3633345727604ad21eec8439fa1ba4c892bc
Joshua Marshall Moore <moore.joshua@pm.me>
2020-01-27 10:23:16 -0800

    jquery touch punch

--- docs/js/app.js
+++ docs/js/app.js
@@ -4,20 +4,69 @@ var AppCtrl = App.controller('AppCtrl', ['$scope', async function($scope){
   $scope.output = "Nothing yet";
   
   $scope.graph = document.querySelector('graph-visualization');
-  /*
-  $scope.settings = {
-    attraction     : 4e-1,
-    repulsion      : 1e-2,
-    epsilon        : 1e-3,
-    inner_distance : 9e-3,
-    time_dilation  : 0.1,
-    friction       : 8e-1,
-    gravity        : 1e-1,
-    dampening      : 1e-1,
-    drag           : 1e-3,
-    theta          : 0.25,
-    spread         : 1e4
-  };
-  */
+  var range = function(n){
+    var arr = [];
+    for(var i=0; i<n; i++){
+      arr.push(i);
+    }
+
+    return arr;
+  }
+
+  $scope.i = 0;
+  $scope.addVertex = function add_vertex(){
+    var vertex = document.createElement('graph-vertex');
+    vertex.id = `id-${$scope.i++}`;
+    vertex.face = randomColor();
+    vertex.size = 5;
+
+    $scope.graph.appendChild(vertex)
+
+    vertex.onclick = function(e){
+      select(e.target);
+    }
+
+    vertex.ondblclick = function(e){
+      centerAround(e.target);
+    }
+  }
+
+  $scope.addVertices = function(){
+    var n = parseInt(prompt('How many? ', 100));
+    var i = 0; 
+    var t = setInterval(function(){
+      $scope.addVertex();
+      if(i++ >= n){
+        clearInterval(t);
+      }
+    }, 100)
+
+
+  }
+
+  $scope.addEdge = function(){
+    var vertices = $scope.graph.querySelectorAll('graph-vertex');
+    var l = vertices.length;
+    var source = `#id-${Math.round(Math.random() * l)}`;
+    var target = `#id-${Math.round(Math.random() * l)}`;
+
+    var edge = document.createElement('graph-edge');
+    edge.color = 'black';
+    edge.source = source;
+    edge.target = target;
+
+    $scope.graph.appendChild(edge);
+  }
+
+  $scope.addEdges = function(){
+    var n = parseInt(prompt('How many edges?', 100));
+    var i = 0; 
+    var t = setInterval(function(){
+      $scope.addEdge();
+      if(i++ >= n){
+        clearInterval(t);
+      }
+    }, 100)
+  }
 
 }]);
